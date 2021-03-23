779b3a5fe12e3996d398067fefd43c561562d2cf
Jakub <jakubgradzewicz1309@gmail.com>
2021-03-23 10:32:09 +0100

    7 1

---
+++ CP/codeforces/48NewYearandHurry.py
@@ -0,0 +1,15 @@
+h = input().split(" ")
+n = int(h[0])
+k = int(h[1])
+
+time = 240 - k
+result = 0
+
+for i in range(1, n+1):
+    if time - (5*i) >= 0:
+        time = time - (5*i)
+        result += 1
+    else:
+        break
+
+print(result)
