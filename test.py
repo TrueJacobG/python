4869192acf306c879189233dd485d5a5adfebf49
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-05-23 19:42:42 +0200

    Sudoku solver part 1 - game

--- test.py
+++ test.py
@@ -2,6 +2,14 @@ def isequal(l):
     return len(set(l)) <= 1
 
 
-l = [1, 1, 1, 1]
+def GridColor(x, y):
+    colors = [(229, 232, 53), (194, 196, 43), (164, 166, 36), (55, 237, 94),
+              (47, 204, 81), (38, 166, 66), (54, 211, 235), (46, 181, 201), (39, 158, 176)]
+    comp = [[0, 1, 2], [3, 4, 5], [6, 7, 8, 9]]
+    for i in range(3):
+        for j in range(3):
+            if j == x and i == y:
+                return (j, i)
 
-print(isequal(l))
+
+print(GridColor(2, 2))
