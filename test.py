f15caf4a7894815af7813be9ebda30b8915b3e8a
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-05-24 19:10:44 +0200

    test

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ def isequal(l):
     return len(set(l)) <= 1
 
 
-def GridColor(x, y):
-    colors = [(229, 232, 53), (194, 196, 43), (164, 166, 36), (55, 237, 94),
-              (47, 204, 81), (38, 166, 66), (54, 211, 235), (46, 181, 201), (39, 158, 176)]
-    comp = [[0, 1, 2], [3, 4, 5], [6, 7, 8, 9]]
-    for i in range(3):
-        for j in range(3):
-            if j == x and i == y:
-                return (j, i)
+def kidsWithCandies(candies: list[int], extraCandies: int) -> list[bool]:
+    l: list[bool] = []
+    for c in candies:
+        if c + extraCandies > max(candies):
+            l.append(True)
+        else:
+            l.append(False)
+    return l
 
 
-print(GridColor(2, 2))
+print(kidsWithCandies([2, 3, 5, 1, 3], 3))
