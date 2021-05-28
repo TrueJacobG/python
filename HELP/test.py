500d5de056b6e3f2b73ca58799e437e9681c5383
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-05-28 10:02:52 +0200

    test

--- HELP/test.py
+++ HELP/test.py
@@ -1,14 +1,1 @@
-import time
-
-
-def gsum():
-    start = time.time()
-    s = 0
-    for i in range(100000000):
-        s += i
-    print(time.time()-start)
-    return s
-
-
-if __name__ == '__main__':
-    gsum()
+print("TYMEK")
