ec9ace28050bcf4c6b1b5cbc5c0010e96f29315a
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-05-31 16:43:25 +0200

    .

--- COMPETITIVE PROGRAMMING/leetcode/1test.py
+++
@@ -1,9 +0,0 @@
-def numIdenticalPairs(nums: list[int]) -> int:
-    r: int = 0
-    for i in range(len(nums)):
-        if nums[i] in nums[i+1:]:
-            r += nums[i+1:].count(nums[i])
-    print(r)
-
-
-numIdenticalPairs([1, 2, 3, 1, 1, 3])
--- HELP/test.py
+++ HELP/test.py
@@ -1,1 +1,53 @@
-print("TYMEK")
+def checkZeroOnes(s: str) -> bool:
+    nulls: int = 0
+    ones: int = 0
+    nulls_max: int = 0
+    ones_max: int = 0
+
+    if s[0] == "1" and len(s) == 1:
+        return True
+    if s[0] == "0" and len(s) == 1:
+        return False
+
+    #if s[0] == "1":
+    #    ones += 1
+    if s[0] == "0":
+        nulls += 1
+
+    for i in range(1, len(s)):
+        if s[i-1] == "1" and s[i] == "1":
+            if nulls_max < nulls:
+                nulls_max = nulls
+                nulls = 0
+            ones += 1
+        if s[i-1] == "0" and s[i] == "0":
+            if ones_max < ones:
+                ones_max = ones
+                ones = 0
+            nulls += 1
+
+        if s[i-1] == "0" and s[i] == "1":
+            if nulls_max < nulls:
+                nulls_max = nulls
+                nulls = 0
+            ones += 1
+
+        if s[i-1] == "1" and s[i] == "0":
+            if ones_max < ones:
+                ones_max = ones
+                ones = 0
+            nulls += 1
+
+    if nulls_max < nulls:
+        nulls_max = nulls
+    if ones_max < nulls:
+        ones_max = ones
+    print("N_MAX", nulls_max)
+    print("O_MAX", ones_max)
+
+    if ones_max > nulls_max:
+        return True
+    return False
+
+
+checkZeroOnes("0111010011")
