5fe006d90f5224e3d3f6d82dc8278e9f3c965589
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-12 13:17:34 +0200

    .

--- HELP/test.py
+++ HELP/test.py
@@ -1,13 +1,13 @@
-from collections import OrderedDict
+def numberOfMatches(n: int) -> int:
+    matches: int = 0
+    while n > 1:
+        if n % 2 == 0:
+            matches += n//2
+            n -= n//2
+        else:
+            matches += (n-1)//2
+            n = n - (n-1)//2
+    return matches
 
 
-def sortSentence(s: str) -> str:
-    result = OrderedDict()
-
-    for word in s.split():
-        result[int(word[len(word)-1])-1] = word[:len(word)-1]
-
-    return " ".join(dict(sorted(result.items())).values())
-
-
-print(sortSentence("is2 sentence4 This1 a3"))
+print(numberOfMatches(7))
