073a358400e079b181ceeddf4ca42ae8ff86b23f
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-12 14:42:24 +0200

    ML - 1st project

--- ML/1stproject/main.py
+++ ML/1stproject/main.py
@@ -1,13 +1,45 @@
 # %%
-# jupyter cause why not
 
+# jupyter cause why not
+from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
 melbourne = "melb_data.csv"
-
 melbourne_data = pd.read_csv(melbourne)
-
 # info -> print
 melbourne_data.describe()
+
+# %%
+
+melbourne_data.columns
+# %%
+
+# remove
+melbourne_data.dropna(axis=0)
+
+# %%
+
+y = melbourne_data.Price
+
+# features -> things that you will use to make predictions
+melbourne_features = ['Rooms', 'Bathroom',
+                      'Landsize', 'Lattitude', 'Longtitude']
+
+X = melbourne_data[melbourne_features]
+X.describe()
+X.head()
+# %%
+
+# create a model
+melbourne_model = DecisionTreeRegressor(random_state=1)
+
+# fit a model
+melbourne_model.fit(X, y)
+
+# %%
+
+# print prediction
+print(X.head())
+print(melbourne_model.predict(X.head()))
 
 # %%
