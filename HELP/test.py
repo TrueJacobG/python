f78987d18beeee0b5bae6445c52b1697cfc2f694
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-12 17:55:02 +0200

    .

--- HELP/test.py
+++ HELP/test.py
@@ -1,12 +1,1 @@
-def returnBetterPermutations(arr, length):
-    result = []
-    starting = 0
-    ending = length
-    while ending <= len(arr):
-        result.append(arr[starting:ending])
-        starting += 1
-        ending += 1
-    return result
-
-
-print(returnBetterPermutations([1, 4, 2, 5, 3], 3))
+import tensorflow
