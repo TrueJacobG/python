cfc29db3d62b7bfbdcffbe97a280d81d693e1645
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-14 14:54:39 +0200

    ML - make predictions

--- ML/1stproject/main.py
+++ ML/1stproject/main.py
@@ -4,7 +4,7 @@
 from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
-melbourne = "melb_data.csv"
+melbourne = "../melb_data.csv"
 melbourne_data = pd.read_csv(melbourne)
 # info -> print
 melbourne_data.describe()
