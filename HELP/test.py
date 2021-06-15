4eff731ac71b011050d3d94e775cf6c586f3c353
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-15 12:22:47 +0200

    .

--- HELP/test.py
+++ HELP/test.py
@@ -1,1 +1,4 @@
-import tensorflow
+letters = "A B C D E F G H I J K L lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z".upper().replace(" ", "")
+
+print("".join(set(sorted(letters))))
+print("".join(sorted("MWCYJTAGZRKFIPSBEXULQHDVON")))
