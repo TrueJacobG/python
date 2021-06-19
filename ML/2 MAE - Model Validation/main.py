3cf7c930473e0b7f4c7164c482f7c7714432d51e
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-19 20:09:38 +0200

    overfitting and underfitting

--- ML/2 MAE - Model Validation/main.py
+++ ML/2 MAE - Model Validation/main.py
@@ -1,9 +1,8 @@
 # %%
 
-
 # error of predction -> error = |actual-predicted|
 
-
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
@@ -29,7 +28,6 @@ print(mean_absolute_error(y, predicted_prices))
 # better validation data
 # train data to make better prediction
 
-from sklearn.model_selection import train_test_split
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
@@ -44,3 +42,27 @@ validated_predictions = melbourne_model.predict(val_X)
 print(mean_absolute_error(val_y, validated_predictions))
 
 # %%
+
+############################
+# underfitting and overfitting
+# how to make your model more accurate
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(
+        max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    predictions = model.predict(val_X)
+    return mean_absolute_error(val_y, predictions)
+
+
+for max_leaf_nodes in [10, 100, 500, 1000, 2000]:
+    mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print(f"Max leaf nodes: {max_leaf_nodes} Mean Absolute Value: {mae}")
+
+# the best deepness is 500
+# the best number of leaves -> 500
+
+# overfitting -> a lot of leaves, week with new data
+# underfitting -> less accurate
+# golden mean -> 😎😎😎
