dd52e78785398ce8ef2f2064fa0ad48afad71da9
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-20 19:33:07 +0200

    .

--- HELP/test.py
+++ HELP/test.py
@@ -1,10 +1,14 @@
-class Test:
-    def __init__(self):
-        self.name = "Ro"
+def largestAltitude(gain):
+    actual = 0
+    maximum = 0
+    helper = []
+    for i in range(len(gain)):
+        helper.append(actual)
+        actual = actual + gain[i]
+        maximum = max(maximum, actual)
+        print(maximum)
+    # print(helper)
+    return maximum
 
-    def cout(self):
-        print(self.name)
 
-
-o = Test()
-o.cout()
+print(largestAltitude([-4, -3, -2, -1, 4, 3, 2]))
