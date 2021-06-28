b921d2cc997e3a640dc36d413f9da59f356dab5c
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-06-28 19:38:33 +0200

    Multiprocessing, multithreading

---
+++ MULTIPROCESSING/main.py
@@ -0,0 +1,29 @@
+from threading import Thread
+from os import cpu_count
+import time
+
+
+def test_func(a):
+    y = []
+    for i in range(a):
+        for x in range(a):
+            y.append(x*i)
+
+
+threads = []
+num_threads = cpu_count()
+
+# create processes
+for i in range(num_threads):
+    t = Thread(target=test_func, args=(100,))
+    threads.append(t)
+
+# start
+for t in threads:
+    t.start()
+
+# waiting for thread
+for t in threads:
+    t.join()
+
+print("end")
