8476fdbb90fd4c54a6f00a936669b5432848f853
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-07-08 16:19:02 +0200

    Codeforces - Combination Lock

---
+++ COMPETITIVE PROGRAMMING/codeforces/140CombinationLock.py
@@ -0,0 +1,20 @@
+from math import fabs
+
+
+def minDistance(n1, n2):
+    change = {6: 4, 7: 3, 8: 2, 9: 1}
+    distance = int(fabs(int(n1) - int(n2)))
+    if distance <= 5:
+        return distance
+    return change[distance]
+
+
+n = int(input())
+locked = input()
+unlocked = input()
+
+result = 0
+for i in range(n):
+    result += minDistance(locked[i], unlocked[i])
+
+print(result)
