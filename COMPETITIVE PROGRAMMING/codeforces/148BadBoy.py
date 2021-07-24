2a912a88670fadc9031035ce359e9c5bbb25bef0
Jakub <jakubgradzewicz1309@gmail.com>
2021-07-24 18:07:41 +0200

    Codeforces - Bad Boy

---
+++ COMPETITIVE PROGRAMMING/codeforces/148BadBoy.py
@@ -0,0 +1,9 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, m, i, j = [int(x) for x in input().split()]
+        print(f"1 {m} {n} 1")
+
+
+if __name__ == "__main__":
+    main()
