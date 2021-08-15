f9c98368dd7781fcb7c86ccb8b5f4f3819713050
Jakub <jakubgradzewicz1309@gmail.com>
2021-08-15 12:42:45 +0200

    get view from webcam

---
+++ PROJECTS/camera_cv2/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    cv2.imshow('webcam', img)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
