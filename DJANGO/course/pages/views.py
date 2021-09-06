9c06757d191d655f2f37aa1d00f7d7fc19977324
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-09-06 13:21:17 +0200

    if statement - django

--- DJANGO/course/pages/views.py
+++ DJANGO/course/pages/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
     my_context = {
         "my_title": "Main Page - Home",
-        "my_list": [123, 456, 789],
+        "my_list": [1, 2, 3],
     }
     return render(request, "home.html", my_context)
 
