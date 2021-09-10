994706bba38814c0bf53ee0c1e7661b41fa94635
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-09-10 11:12:27 +0200

    forms

--- DJANGO/course/djangoCourse/urls.py
+++ DJANGO/course/djangoCourse/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact_view),
     path('product/', product_detail_view),
+    path('create/', product_create_view),
     path('home/', home_view),
     path('', home_view),
 ]
--- DJANGO/course/products/views.py
+++ DJANGO/course/products/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 # Create your views here.
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    content = {
+        "form": form,
+    }
+    return render(request, "products/product_create.html", content)
 
 
 def product_detail_view(request):
