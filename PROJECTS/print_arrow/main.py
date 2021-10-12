07eea56ed7d0676eda1617aba238e2135972096c
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-10-12 18:41:49 +0200

    arrow up and arrow down

--- PROJECTS/print_arrow/main.py
+++ PROJECTS/print_arrow/main.py
@@ -1,42 +1,51 @@
 def print_arrow(size, direction="right"):
+
     changed = False
+    symbol = "#"
 
-    if size % 2 == 0:
-        size += 1
+    if direction == "right" or direction == "left":
+        if size % 2 == 0:
+            size += 1
 
-    if direction == "right":
-        deleter = size//2
-        d = -1
-    else:
-        deleter = 0
-        d = 1
-
-    for y in range(size):
-        if y <= size//2:
-            line = " " * (size//2-deleter-y*d)
+        if direction == "right":
+            deleter = size//2
+            d = -1
         else:
-            line = " " * (y*d-size//2*d+deleter)
+            deleter = 0
+            d = 1
+
         for x in range(size):
-            line += "#"
-        print(line)
+            if x <= size//2:
+                line = " " * (size//2-deleter-x*d)
+            else:
+                line = " " * (x*d-size//2*d+deleter)
+            for _ in range(size):
+                line += symbol
+            print(line)
+    else:
+        if direction == "down":
+            step = (size * 2) + 1
+            d = -1
+            deleter = size
+        else:
+            step = 1
+            d = 1
+            deleter = 0
 
-
-"""
-4   0
-3   1
-2   2
-1   3
-0   4
-1   3
-2   2
-3   1
-4   0
-
-"""
+        for x in range(size+1):
+            line = " " * (size-deleter - x * d)
+            for _ in range(step):
+                line += symbol
+            print(line)
+            step += 2 * d
 
 
 def main():
-    print_arrow(size=10, direction="right")
+    # print_arrow(size=10, direction="down")
+    # print_arrow(size=10, direction="up")
+    # print_arrow(size=10, direction="right")
+    # print_arrow(size=10, direction="left")
+    print("DONE")
 
 
 if __name__ == '__main__':
