856125643f0f19fe9e7a31a07b6ca9b4e82cf5fc
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-10-22 13:54:59 +0200

    match case statement

---
+++ PROJECTS/match_statement/main.py
@@ -0,0 +1,13 @@
+def main():
+    x = 2
+    match x:
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+
+
+if __name__ == '__main__':
+    main()
