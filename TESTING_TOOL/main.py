1ee99658a93c233ed47a54911f02a7ca2d5922c9
TrueJacobG <jakubgradzewicz1309@gmail.com>
2021-10-23 20:08:56 +0200

    rearranging code

--- TESTING_TOOL/main.py
+++ TESTING_TOOL/main.py
@@ -1,50 +1,7 @@
 import argparse
 import sys
 import subprocess
-from lib import get_raw_filename, to_type
-
-
-def get_functions_in_py(file_txt):
-    functions_in_py = []
-    funcs_count = file_txt.count("def")
-
-    i = 0
-    while funcs_count != 0:
-        func = []
-        df = file_txt.index("def", i)
-        bracket_open = file_txt.index("(", i)
-        bracket_close = file_txt.index(")", i)
-        arrow = file_txt.index(">", i)
-        colon = file_txt.index(":", arrow)
-
-        func.append(file_txt[df+4:bracket_open])
-
-        args_types = ""
-        args = file_txt[bracket_open+1:bracket_close]
-        args_colons = args.count(":")
-        i = 0
-        last_comma_i = -2
-        comma_i = -1
-        while args_colons != 0:
-            args_colon_i = args.index(":", i)
-            if last_comma_i != comma_i:
-                comma_i = args.index(",", i)
-                last_comma_i = comma_i
-            else:
-                comma_i = len(args)
-
-            args_types += args[args_colon_i+2:comma_i] + ";"
-
-            i = comma_i
-            args_colons -= 1
-
-        func.append(args_types + file_txt[arrow+2:colon])
-        functions_in_py.append(func)
-
-        i = colon
-        funcs_count -= 1
-
-    return functions_in_py
+from lib import get_raw_filename, to_type, say, get_args_and_results, py_file_scraping
 
 
 def import_python_file():
@@ -67,28 +24,9 @@ def import_python_file():
         file_txt = f.read()
 
     # get functions from file.py
-    functions_in_py = get_functions_in_py(file_txt)
+    functions_in_py = py_file_scraping(file_txt)
 
     return info, functions_in_py
-
-
-def get_args_results(line, types):
-    line = line.split(";")
-    types = types.split(";")
-
-    args = []
-    results = []
-    i = 0
-
-    for arg in line[0].split(","):
-        args.append(to_type(arg, types[i]))
-        i += 1
-
-    for res in line[1].split(","):
-        results.append(to_type(res, types[i]))
-        i += 1
-
-    return args, results
 
 
 def import_test_file(name):
@@ -96,7 +34,7 @@ def import_test_file(name):
         with open(name+".test", 'r') as f:
             file_txt = f.readlines()
     except:
-        print("\033[93m" + f"ERROR! NO .py.test FILE IN DIRECTORY!" + "\033[0m")
+        say(f"ERROR! NO .py.test FILE IN DIRECTORY!", "y")
         exit()
 
     types_lst = []
@@ -121,7 +59,7 @@ def import_test_file(name):
             need_name = True
             continue
 
-        args, res = get_args_results(line.strip(), types)
+        args, res = get_args_and_results(line.strip(), types)
 
         expectations.append([func, args, res])
 
@@ -132,17 +70,15 @@ def testing_with_mypy(filename):
     try:
         x = subprocess.run(["mypy", filename])
     except:
-        print("\033[91m" +
-              f"YOU HAVE TO INSTALL MYPY" + "\033[0m")
+        say(f"YOU HAVE TO INSTALL MYPY", "r")
         exit()
 
     if x.returncode != 0:
-        print("\033[91m" +
-              f"MYPY DETECTED TYPE ERROR IN {filename}" + "\033[0m")
+        say(f"MYPY DETECTED TYPE ERROR IN {filename}", "g")
         exit()
 
 
-def vis_type_inputs(expects):
+def visualize_input_types(expects):
     for i in range(len(expects)):
         func = expects[i][0]
         line = []
@@ -155,20 +91,18 @@ def vis_type_inputs(expects):
 
         args = expects[i][1] + expects[i][2]
 
-        print(
-            "\033[94m" + f"{args} are {line}" + "\033[0m")
+        say(f"{args} are {line}", "b")
 
 
 def testing_test_file(in_py, in_test, expects, flag):
     if in_py != in_test:
         for i in range(0, len(in_test)):
             if in_py[i][1] != in_test[i][1]:
-                print(
-                    "\033[91m" + f"TYPE ERROR! {in_test[i][0]} -> {in_test[i][1]} SHOULD BE EQUAL TO {in_py[i][1]}" + "\033[0m")
+                say(f"TYPE ERROR! {in_test[i][0]} -> {in_test[i][1]} SHOULD BE EQUAL TO {in_py[i][1]}", "r")
                 exit()
     if flag:
-        vis_type_inputs(expects)
-        print("\033[94mTYPES and FUNCS IN .py AND .py.test ARE EQUAL\033[0m")
+        visualize_input_types(expects)
+        say("TYPES and FUNCS IN .py AND .py.test ARE EQUAL", "b")
 
 
 def test_function(info, expectations):
@@ -182,22 +116,21 @@ def test_function(info, expectations):
         try:
             result = getattr(module, x[0])(*x[1])
         except AttributeError:
-            print(
-                "\033[93m" + f"You put wrong function in {info['filename']}.test" + "\033[0m")
+            say(f"You put wrong function in {info['filename']}.test", "y")
             exit()
 
         if [result] != x[2]:
             ok = False
-            print(
-                f"\033[91mWARNING! {x[0]} -> {x[1]} should give {x[2]}, but instead gave {[result]}\033[0m")
+            say(
+                f"WARNING! {x[0]} -> {x[1]} should give {x[2]}, but instead gave {[result]}", "r")
             if info["live"]:
                 break
 
         if info["live"]:
-            print(f"\033[92m{x[0]} args->{x[1]} res->{x[2]}\033[0m")
+            say(f"{x[0]} args->{x[1]} res->{x[2]}", "g")
 
     if ok:
-        print("\033[92mEVERYTHING OK!\033[0m")
+        say("EVERYTHING OK!", "g")
 
 
 def main():
