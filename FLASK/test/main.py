2824adbfb9b2d0eb9637a3abd964ccf61acef6d3
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-01-13 20:24:55 +0100

    css

--- FLASK/test/main.py
+++ FLASK/test/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "<h1> Hello World! </h1>"
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
