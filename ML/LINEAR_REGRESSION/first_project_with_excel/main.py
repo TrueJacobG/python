6b9e3bad961a812dae5a5f3b25cbdd5a03188143
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-01-15 21:10:16 +0100

    linear regression with excel

---
+++ ML/LINEAR_REGRESSION/first_project_with_excel/main.py
@@ -0,0 +1,29 @@
+# %%
+import tensorflow as tf
+import pandas as pd
+
+# %%
+dataframe = pd.read_excel("data.xlsx", usecols="A:B")
+
+sizes = dataframe["HouseSize"].to_numpy()
+prices = dataframe["Price"].to_numpy()
+
+# %%
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+
+model.compile(optimizer="sgd", loss=tf.keras.losses.MeanAbsoluteError())
+
+# %%
+when_stop = tf.keras.callbacks.EarlyStopping(
+    monitor="loss", mode="min", patience=5)
+
+# patience - how many echos it should do try after reaching the halt
+
+model.fit(sizes, prices, epochs=10000,
+          callbacks=when_stop)
+
+
+# %%
+print(model.predict([300]))
+
+# %%
